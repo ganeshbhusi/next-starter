d05b0b4df9476a13a1522d37c7cb9fb0e48101ab
Ganesh Bhusi <ganeshpaul999@gmail.com>
2021-08-10 23:11:19 +0530

    added network call successfully

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Head from "next/head";
 import Login from "./login/login";
 import CheckImage from "./Images/CheckImage";
 import {getSortedPostsData} from "../lib/posts";
+import {getSomeOnlineContent} from "../lib/fetchOnlineData";
 
 export default function Home(props) {
-  console.log(props);
+  const {allPostsData = [], onlineContent = []} = props;
   return (
     <div>
       <Head>
@@ -21,19 +22,35 @@ export default function Home(props) {
         Open Person
       </Link>
       <CheckImage />
-      <div>
-        <h3>About Me</h3>
-        <p>Some short description about me</p>
-      </div>
+      <p>Online Content Count: {onlineContent.length}</p>
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({id, date, title}) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
 
 export async function getStaticProps() {
+  const onlineContent = await getSomeOnlineContent();
   const allPostsData = getSortedPostsData();
+  console.log(onlineContent);
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+  const data = await res.json()
+
+
   return {
-    props: {
-      allPostsData,
-    },
-  };
+    props: { onlineContent: onlineContent, onlineContent2: data, allPostsData },
+  }
 }
